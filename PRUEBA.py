5646ad6e4654f5f7fd277ec69f75f49f948dc3da
CRGdev11 <crizhan10@gmail.com>
2022-12-19 00:46:22 -0600

    19/12/22

--- PRUEBA.py
+++ PRUEBA.py
@@ -7,9 +7,16 @@ class switch:
     puerto = 22
 
     
-
+#print('funcionando')
 
 class router:
-    hostname = '172.16.111.58'
     password = '******'
     puerto = 22
+    hostname = '172.16.111.58'
+    print(hostname)
+
+
+    #def obtenerhostname():
+
+
+router()
