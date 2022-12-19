aa757df8c92b883ef4a9514e56dbb71d10edd361
CRGdev11 <crizhan10@gmail.com>
2022-12-18 21:46:16 -0600

    primer commit

---
+++ nornir_main.py
@@ -0,0 +1,11 @@
+from nornir import InitNornir
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko.tasks import netmiko_send_command, netmiko_send_config
+from nornir_napalm.plugins.tasks import napalm_cli
+
+nr = InitNornir(
+  config_file = 'config.yaml', dry_run= True)
+
+result = nr.run(task = napalm_cli, command = ['sho interface brief'])
+
+print_result(result)
