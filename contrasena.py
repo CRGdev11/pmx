0c1c45553ef115daaa76e7a72b878fe126132ae2
CRGdev11 <crizhan10@gmail.com>
2022-12-27 01:13:35 -0600

    26/12/22

--- contrasena.py
+++ contrasena.py
@@ -1,17 +1,19 @@
 from getpass import getpass
 
 class inicioSesion():
-    usuario = None
 
     def inicio_sesion(self):
         self.inicio_sesion = input(" ¿Quieres iniciar sesion? y/n ")
-
+        
     def obtener_usuario(self):
-        self.usuario = getpass("\n introduzca el usuario: ")
+        self.usuario = input("\n introduzca el usuario: ")
         print(self.usuario)
 
     def obtener_contrasena(self):
         self.contrasena = getpass("\n introduzca la contrasena: ")
+        print(self.contrasena)
 
-autenticacion = inicioSesion
-autenticacion.obtener_usuario()
+autenticacion = inicioSesion()
+#autenticacion.inicio_sesion()
+#autenticacion.obtener_usuario()
+#autenticacion.obtener_contrasena()
---
+++ objeto.py
@@ -0,0 +1,10 @@
+class inicioSesion():
+    marca = "xd"
+
+    def prueba1(self):
+     self.marca = ":v"
+
+
+objeto = inicioSesion()
+objeto.prueba1()
+print(objeto.marca)
